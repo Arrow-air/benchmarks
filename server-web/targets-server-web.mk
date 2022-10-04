61f67b84b00efed35de96a3f07974ec69345ebd2
Owlot <95369289+owlot@users.noreply.github.com>
2022-10-04 07:49:03 +0200

    chore(example): adding docker compose to run examples

--- server-web/targets-server-web.mk
+++ server-web/targets-server-web.mk
@@ -1,5 +1,16 @@
-DOCKER_BUILD_PATH=../
-DOCKER_NAME=arrow-$(PACKAGE_NAME)
+DOCKER_BUILD_PATH = ../
+DOCKER_NAME       = arrow-$(PACKAGE_NAME)
+DOCKER_PORT       = 8000
+HOST_PORT         = 8080
+HOSTNAME          = $(PACKAGE_NAME)-run
 
 example: check-cargo-registry docker-pull
-	@$(call cargo_run,run --example endpoints)
+	@docker compose \
+		-f ../docker-compose.yml \
+		run \
+		--workdir=/usr/src/app/${PACKAGE_NAME} \
+		--user `id -u`:`id -g` \
+		--rm \
+		-e CARGO_INCREMENTAL=1 \
+		-e RUSTC_BOOTSTRAP=0 \
+		example
